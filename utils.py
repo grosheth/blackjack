e06eb154782d1c286b16d2f6ec2712e49f3327b7
salledelavage <alextrington@gmail.com>
2022-10-16 10:39:26 -0400

    commit automatique

--- utils.py
+++ utils.py
@@ -4,7 +4,17 @@ from art import *
 
 def play():
     cards = ['1','2','3','4','5','6','7','8','9','10','J','Q','K','A']
+    x = 0
+    for value in cards:
+        if x < 42:
+            cards.append(value)
+        else:
+            break
+        x += 1
+    
+    print(cards)
     points = 0
+    dealer_points = 0
     hand = []
     dealer_hand = []
     print("Here is the dealer's first card:")
@@ -12,25 +22,43 @@ def play():
     print("Here is your hand:")
     get_card(cards, hand)
     points = convert(hand, points)
+    dealer_points = convert(dealer_hand, dealer_points)
     while True:
         print(f"you currently have {points}")
+        print(f"The dealer has {dealer_points}")
         print("1. Get another card.")
-        print("2. Keep current cards.")
+        print("2. Stand with current cards.")
         answer = input()
 
         if answer == "1":
             print("Here is your hand:")
             get_card(cards, hand)
+            points = convert(hand, points)
+            if points > 21:
+                print("You have more than 21, you lost")
+                print("1: Replay    2: Quit")
+                exit()
+            win = decision(points, dealer_points)
+            if win:
+                print(f"You won with {points} against the dealer's {dealer_points}")
+                
         elif answer == "2":
             print("Here is the dealer's hand")
             get_card_dealer(cards, dealer_hand)
+            dealer_points = convert(dealer_hand, dealer_points)
+            if dealer_points > 21:
+                print("You Won the dealer has more than 21")
+                print("1: Replay    2: Quit")
+                exit()
         else:
             print("invalid answer")
 
 
 def get_card_dealer(cards, dealer_hand):
 
-    card = cards[randint(0,13)]
+    card = cards[randint(len(cards))]
+    cards.pop(card)
+    print(cards)
     dealer_hand.append(card)
     if len(dealer_hand) < 2:
         for c in range(len(dealer_hand)):
@@ -57,11 +85,13 @@ def get_card(cards, hand):
 
 
 def convert(hand, points):
-    for c in hand:
-        if c.isdigit():
-            c = int(c)
+    print(points)
+    print(hand)
+    if points > 0:
+        if hand[-1].isdigit():
+            c = int(hand[-1])
         else:
-            if c != "A":
+            if hand[-1] != "A":
                 c = 10
             elif points < 21:
                 c = 11
@@ -69,6 +99,26 @@ def convert(hand, points):
                 c = 1
         print(c)
         points += c
+    else:
+        for c in hand:
+            if c.isdigit():
+                c = int(c)
+            else:
+                if c != "A":
+                    c = 10
+                elif points < 21:
+                    c = 11
+                else:
+                    c = 1
+            print(c)
+            points += c
     print(points)
     return points
 
+def decision(points, dealer_points):
+    if points > dealer_points:
+        win = True
+        return win
+    else:
+        win = False
+        return win
